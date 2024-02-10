d235b4d557bea25322efa074be426d763e759a6a
misha <mishavkrpv@gmail.com>
2024-02-11 02:43:00 +0300

    Add DataAccess.Domain interfaces and implementations

---
+++ src/Implementations/Boatman.DataAccess.Domain.Implementations/EfRepository.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification.EntityFrameworkCore;
+using Boatman.DataAccess.Domain.Interfaces;
+using Boatman.Entities.Models;
+
+namespace Boatman.DataAccess.Domain.Implementations;
+
+public class EfRepository<T> : RepositoryBase<T>, IReadRepository<T>, IRepository<T> where T : class, IAggregateRoot
+{
+    public EfRepository(DomainContext dbContext) : base(dbContext)
+    {
+    }
+}
---
+++ src/Interfaces/Boatman.DataAccess.Domain.Interfaces/IReadRepository.cs
@@ -0,0 +1,8 @@
+﻿using Ardalis.Specification;
+using Boatman.Entities.Models;
+
+namespace Boatman.DataAccess.Domain.Interfaces;
+
+public interface IReadRepository<T> : IReadRepositoryBase<T> where T : class, IAggregateRoot
+{
+}
