583c6d1898cf714cfe90e59a921f07558301cbe6
misha <mishavkrpv@gmail.com>
2024-02-12 01:16:30 +0300

    Add owner api controllers project

---
+++ src/Controllers/Boatman.OwnerApi.Controllers/ApartmentController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Boatman.OwnerApi.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class ApartmentController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ApartmentController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+}
