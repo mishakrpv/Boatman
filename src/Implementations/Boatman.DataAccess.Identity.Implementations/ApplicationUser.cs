82bd0a752da398bd137c4890fe53da593416e38b
misha <mishavkrpv@gmail.com>
2024-02-11 11:34:48 +0300

    Add DataAccess.Identity implementations

---
+++ src/Implementations/Boatman.DataAccess.Identity.Implementations/ApplicationUser.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Boatman.DataAccess.Identity.Implementations;
+
+public class ApplicationUser : IdentityUser
+{
+}
---
+++ src/Implementations/Boatman.DataAccess.Identity.Implementations/IdentityContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Boatman.DataAccess.Identity.Implementations;
+
+public class IdentityContext : IdentityDbContext<ApplicationUser>
+{
+    public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
