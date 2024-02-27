e4e08b7f0c469fe86f57ea48835161b6c95f4172
misha <mishavkrpv@gmail.com>
2024-02-27 22:58:04 +0300

    Fix typo

--- backend/src/Boatman.WebHost/Configurations/ConfigureInterfaceAdapters.cs
+++ backend/src/Boatman.WebHost/Configurations/ConfigureInterfaceAdapters.cs
@@ -9,6 +9,7 @@ using Boatman.Emailing.Implementations.SendGrid;
 using Boatman.Emailing.Interfaces;
 using Boatman.Logging.Implementations;
 using Boatman.Logging.Interfaces;
+using Boatman.ProfileService.Interfaces;
 
 namespace Boatman.WebHost.Configurations;
 
@@ -20,6 +21,7 @@ public static class ConfigureInterfaceAdapters
         services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
 
         services.AddScoped<IAuthService, AuthService.Implementations.Jwt.Identity.AuthService>();
+        services.AddScoped<IProfileService, ProfileService.Implementations.Identity.ProfileService>();
         services.AddScoped<IBlobStorage, AmazonS3BlobStorage>();
         services.AddTransient<IEmailSender, SendGridEmailSender>();
 
