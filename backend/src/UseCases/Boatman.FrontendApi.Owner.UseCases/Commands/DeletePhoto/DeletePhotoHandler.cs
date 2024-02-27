b4aef7d7e0cbde8354176c29154aeac31cabb4d2
misha <mishavkrpv@gmail.com>
2024-02-27 22:18:55 +0300

    Add delete apartment photo feature

---
+++ backend/src/UseCases/Boatman.FrontendApi.Owner.UseCases/Commands/DeletePhoto/DeletePhotoHandler.cs
@@ -0,0 +1,39 @@
+﻿using Boatman.DataAccess.Interfaces;
+using Boatman.DataAccess.Interfaces.Specifications;
+using Boatman.Entities.Models.ApartmentAggregate;
+using Boatman.Utils.Models.Response;
+using MediatR;
+
+namespace Boatman.FrontendApi.Owner.UseCases.Commands.DeletePhoto;
+
+public class DeletePhotoHandler : IRequestHandler<DeletePhoto, Response>
+{
+    private readonly IRepository<Apartment> _apartmentRepo;
+
+    public DeletePhotoHandler(IRepository<Apartment> apartmentRepo)
+    {
+        _apartmentRepo = apartmentRepo;
+    }
+
+    public async Task<Response> Handle(DeletePhoto request, CancellationToken cancellationToken)
+    {
+        var spec = new ApartmentWithPhotosSpecification(request.ApartmentId);
+        var apartment = await _apartmentRepo.FirstOrDefaultAsync(spec);
+
+        if (apartment == null)
+            return new Response
+            {
+                StatusCode = 404,
+                Message = "Apartment not found."
+            };
+        
+        apartment.DeletePhoto(request.PhotoId);
+
+        await _apartmentRepo.UpdateAsync(apartment);
+
+        return new Response
+        {
+            Message = "Photo has been deleted."
+        };
+    }
+}
