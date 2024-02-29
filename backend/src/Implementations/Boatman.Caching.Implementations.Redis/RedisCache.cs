34a24a2ff48e9c08dc5b130b7cc3d5f41d18244c
misha <mishavkrpv@gmail.com>
2024-02-29 17:26:40 +0300

    Add redis cache adapter

---
+++ backend/src/Implementations/Boatman.Caching.Implementations.Redis/RedisCache.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json;
+using Boatman.Caching.Interfaces;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Boatman.Caching.Implementations.Redis;
+
+public class RedisCache : ICache
+{
+    private readonly IDistributedCache _cache;
+
+    public RedisCache(IDistributedCache cache)
+    {
+        _cache = cache;
+    }
+
+    public async Task<T?> GetAsync<T>(string key, CancellationToken cancellationToken = default)
+    {
+        var valueAsString = await _cache.GetStringAsync(key);
+
+        if (valueAsString != null)
+        {
+            return JsonSerializer.Deserialize<T>(valueAsString);
+        }
+
+        return default;
+    }
+
+    public async Task SetAsync<T>(string key, T value, CancellationToken cancellationToken = default)
+    {
+        var valueAsString = JsonSerializer.Serialize(value);
+
+        await _cache.SetStringAsync(key, valueAsString);
+    }
+
+    public async Task RefreshAsync(string key, CancellationToken cancellationToken = default)
+    {
+        await _cache.RefreshAsync(key);
+    }
+
+    public async Task RemoveAsync(string key, CancellationToken cancellationToken = default)
+    {
+        await _cache.RemoveAsync(key);
+    }
+}
---
+++ backend/src/Interfaces/Boatman.Caching.Interfaces/ICache.cs
@@ -0,0 +1,12 @@
+﻿namespace Boatman.Caching.Interfaces;
+
+public interface ICache
+{
+    Task<T?> GetAsync<T>(string key, CancellationToken cancellationToken = default);
+
+    Task SetAsync<T>(string key, T value, CancellationToken cancellationToken = default);
+
+    Task RefreshAsync(string key, CancellationToken cancellationToken = default);
+
+    Task RemoveAsync(string key, CancellationToken cancellationToken = default);
+}
